d347d2e46fc86d891c3368cd57c7d6639379efcc
Augusto Occhiuzzi <96669705+augustoocch@users.noreply.github.com>
2022-04-10 15:06:00 -0300

    Update ConexionBase.java

--- Conexion/ConexionBase.java
+++ Conexion/ConexionBase.java
@@ -11,7 +11,7 @@ public class ConexionBase {
      
         private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true"; 
         private static final String JDBC_USER = "root";
-        private static final String JDBC_PASSWORD = "Bigua205";
+        private static final String JDBC_PASSWORD = "xxxxxxxxxx";
         
         public static Connection conexion() throws SQLException {
             
